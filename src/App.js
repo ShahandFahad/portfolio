e5afae165f06d87bfb5cc46da0b6f01e17b405ec
ShahandFahad <dev.shahfahad@gmail.com>
2024-06-02 11:39:57 +0500

    Initial commit for version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-gray-950 text-gray-200 min-h-screen selection:bg-gray-400 selection:text-gray-200">
+      <header className="">
+        <h1 className="lg:text-4xl font-bold"> This is Test</h1>
       </header>
     </div>
   );
