ee8a44632f2bcb2a5547f60cd7e0132e7ffe6dcf
ShahandFahad <dev.shahfahad@gmail.com>
2024-06-04 20:51:12 +0500

    Added Certification Section

---
+++ src/components/Sections/Certification/CertificationSection.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import SectionLayout from "../../../layout/SectionLayout";
+import { DataContext } from "../../../context/Context";
+import CardLayout from "../../../layout/CardLayout";
+import Image from "../../Card/Image/Image";
+import Body from "../../Card/Body/Body";
+import Title from "../../Card/Title/Title";
+
+/**
+ *
+ * @returns List of certificate cards
+ */
+export default function CertificationSection() {
+  const { certification } = useContext(DataContext).sections;
+
+  return (
+    <SectionLayout id={certification.id} title={certification.title}>
+      {/* List of certificate Cards */}
+      <ol class="group/list">
+        {certification.field.map((certificate) => (
+          <li className="mb-12">
+            <CardLayout hasImage={true} certificationSection={true}>
+              <Image image={certificate.image} certificateimage={true} />
+              <Body certificateimage={true}>
+                <Title
+                  title={certificate.title}
+                  link={certificate.link}
+                  date={certificate.date}
+                  icon={certificate.icon}
+                />
+              </Body>
+            </CardLayout>
+          </li>
+        ))}
+      </ol>
+    </SectionLayout>
+  );
+}
