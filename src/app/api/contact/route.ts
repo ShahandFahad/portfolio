b1fdd518422d63ac589a6ea16caa33ec82ad7692
Fahad <79867157+ShahandFahad@users.noreply.github.com>
2025-03-25 14:27:14 +0500

    fix resend email

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,18 +1,42 @@
 import { EmailTemplate } from "@/app/components/EmailTemplate";
 import { NextRequest, NextResponse } from "next/server";
+import { ReactNode } from "react";
 import { Resend } from "resend";
 
+interface ResendEmailProps {
+    from: string;
+    to: string;
+    subject: string;
+    react: ReactNode;
+}
+
+// POST request to 'Resend' for sending email
 export const POST = async (req: NextRequest) => {
     const body = await req.json();
-    body.to = process.env.TO; // add to field
 
     // Destrcture the client side request
-    const { from, subject, message, to } = body;
+    const { from, subject, message } = body;
 
     // validate fields
-    if (!from || !subject || !message || !to) {
+    if (!from || !subject || !message) {
         return NextResponse.json(
             { error: "Provide all the fields: from, subject and message" },
+            { status: 400 }
+        );
+    }
+
+    // define 'Resend' email options
+    const resendEmailOpt = <ResendEmailProps>{
+        to: process.env.TO,
+        from: process.env.FROM,
+        subject: subject,
+        react: EmailTemplate({ from, message }),
+    }
+
+    // check envs
+    if (!resendEmailOpt.to || !resendEmailOpt.from || !process.env.RESEND_API_KEY) {
+        return NextResponse.json(
+            { error: "Undefined fields in your .env file or .env file is missing. Please define it or check fields in your .env" },
             { status: 400 }
         );
     }
@@ -22,13 +46,7 @@ export const POST = async (req: NextRequest) => {
 
     // send email via resend
     try {
-
-        const { data, error } = await resend.emails.send({
-            from: process.env.FROM,
-            to: to,
-            subject: subject,
-            react: EmailTemplate({ from, message }),
-        });
+        const { data, error } = await resend.emails.send(resendEmailOpt);
 
         // error response
         if (error) {
