df0e85aa4f293e461dd45088bb66a559383b4880
Fahad <79867157+ShahandFahad@users.noreply.github.com>
2025-06-14 19:12:19 +0500

    feat: add a responsive navbar and sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,53 @@
 'use client'
 
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/Header";
+import { HeroSection } from "@/components/HeroSection";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+
+// Small 1px dots in the background
+export const AnimatedBackground = () => {
+    const controls = useAnimation();
+
+    useEffect(() => {
+        controls.start({
+            backgroundPosition: ["0px", "0px", "-200px", "-200px"],
+            transition: {
+                repeat: Infinity,
+                repeatType: "loop",
+                duration: 20,
+                ease: "linear",
+            },
+        });
+    }, [controls]);
+
+    return (
+        <motion.div
+            className="absolute inset-0 z-0"
+            style={{
+                backgroundImage: `radial-gradient(circle at 1px 1px, rgba(255, 255, 255, 0.15) 1px, transparent 0)`,
+                backgroundSize: "100px 100px",
+            }}
+            animate={controls}
+        />
+    );
+};
 
 export default function Home() {
     return (
-        <main>
-            <Button>
-                Cooking v3.0.0
-            </Button>
-        </main>
+        <div className="min-h-screen bg-gradient-to-r from-black via-slate-900 to-black text-white font-sans relative overflow-hidden">
+            <AnimatedBackground />
+            <div className="relative z-10">
+                {/*Header*/}
+                <Header />
+
+                <main className="">
+                    {/*Sections*/}
+                    <HeroSection />
+                </main>
+
+                {/*Footer*/}
+            </div>
+        </div>
     );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,100 @@
-import { ThemeController } from "./ThemeController";
+"useclient"
+import Link from "next/link";
+import { TypographyH1 } from "./ui/TypographyH1";
+import { Button } from "./ui/button";
+import { SidebarClose, SidebarOpen } from "lucide-react";
+import { useState } from "react";
+
+/*
+  Header
+  01.About
+  02.Experience
+  03.Work
+  04.Contact
+
+  Resume
+*/
 
 export const Header = () => {
+    const [sidebar, setSidebar] = useState('-translate-x-full');
+
+
+    // add and remove '-translate-x-full' class accordingly
+    const handleSidebar = () => {
+
+        if (sidebar === '-translate-x-full') {
+            setSidebar('');
+        }
+
+        if (sidebar !== '-translate-x-full') {
+            setSidebar('-translate-x-full');
+        }
+    };
+
     return (
+        <header className="border sticky top-0 z-50 backdrop-blur border-none py-5 md:px-4">
+            <nav className="container mx-auto px-2 sm:px-4 lg:px-8 flex items-center justify-between h-16 border-slate-900 border shadow-xl border-gray-800">
 
-        <div className="navbar shadow-sm">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Fahad</a>
+                <div className="flex items-center ml-5">
+                    <TypographyH1 text="Shah Fahad" styles="lg:text-4xl md:text-2xl text-1xl  text-orange-500" />
+                </div>
+
+                {/* Navbar Content */}
+                <MenuBarContent style="flex items-center space-x-6 md:block hidden" />
+
+                {/* Open close side bar */}
+                <div className="md:hidden block">
+                    {
+                        sidebar !== '' ?
+                            <SidebarClose onClick={handleSidebar} />
+                            :
+                            <SidebarOpen onClick={handleSidebar} />
+                    }
+                </div>
+
+            </nav>
+
+            {/* Sidebar */}
+            <div className={`${sidebar} absolute bg-black text-white w-56 min-h-screen overflow-y-auto transition-transform transform  ease-in-out duration-300`}
+                id="sidebar">
+                {/* Sidebar Content */}
+                <MenuBarContent style="p-4 flex flex-col space-y-6" />
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#experience">Experience</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li className="hidden md:block"><a href="#contact">Contact</a></li>
-
-                    <li>
-                        <ThemeController />
-                    </li>
-
-                </ul>
-            </div>
-        </div>
-    )
+        </header>
+    );
 };
+
+
+
+// Types of sidebar and navbar
+type MenuBarTypes = {
+    style: string;
+    // handleSidebar: () => void;
+};
+
+// Content of sidebar and navbar
+const MenuBarContent = ({ style }: MenuBarTypes) => {
+    return (
+        <div className={style}>
+            <Link href="#" className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <span className="text-orange-500">01.</span> About
+            </Link>
+
+            <Link href="#" className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <span className="text-orange-500">02.</span> Experience
+            </Link>
+
+            <Link href="#" className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <span className="text-orange-500">03.</span> Work
+            </Link>
+
+            <Link href="#" className="hover:text-orange-500 transition-all duration-300 ease-in-out">
+                <span className="text-orange-500">04.</span> Contact
+            </Link>
+
+            <Button className="bg-black text-orange-500 rounded border border-orange-900 outline hover:border-orange-500 hover:cursor-pointer transition-all duration-300 ease-in-out">
+                Resume
+            </Button>
+        </div>
+    );
+}
