240d4c81251858999de6b4d5cd3a7a04d605c548
Fahad <79867157+ShahandFahad@users.noreply.github.com>
2025-06-22 22:04:51 +0500

    fix: disable lint and type check during build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+// NOTE: Disabling these for production build
+// Checkout This: https://nextjs.org/docs/app/api-reference/config/eslint#disabling-rules
+
+/*
+const eslintConfig = [
+    ...compat.config({
+        extends: [
+            "next/core-web-vitals",
+            "next/typescript"
+        ],
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off", // Disable the TypeScript unused-vars rule
+        },
+    }),
+]
+*/
 
 export default eslintConfig;
